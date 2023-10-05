af9cfa278068d872cf73bd422bc4eb3ed15fa6ad
Aiden Wise <awise1too@gmail.com>
2023-10-04 22:56:21 -0700

    Implemented preOrderTraversal() method

--- TreeDemov1.java
+++ TreeDemov1.java
@@ -58,8 +58,19 @@ class Node{
 	   Prints the value of every node preorder
 	   */
 	   public void preOrderTraversal(Node root){
-		//implement in here
 		   
+		   //base case
+		   if(root == null)
+			   return;
+		   
+		   //print the current node value
+		   System.out.print(root.value + ", ");
+		   
+		   //go left recursively
+		   preOrderTraversal(root.left);
+		   
+		   //go right recursively
+		   preOrderTraversal(root.right);
 	   }
 
 	   
